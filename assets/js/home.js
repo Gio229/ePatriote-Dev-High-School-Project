534c84244d0d74344372650341a070c5f47f9d6c
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2021-08-11 21:45:30 +0100

    setting userInterface and adding animation in the home page

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,14 @@
 const navItems = document.querySelectorAll('.navigation') ;
 const title = document.querySelector('#title') ;
 const subTitle = document.querySelector('#sub-title') ;
-const buton = document.querySelector('#sub-title') ;
+const buton = document.querySelector('.bouton') ;
 
-const transition = new DocumentTimeline({paused: true}) ;
+const transition = new TimelineMax({paused: true}) ;
 
 transition
-.from(title, 1, {y: -100, opacity: 0}) ;
+.from(title, 1, {y: -100, opacity: 0})
+.from(subTitle, 1, {opacity: 0})
+.from(buton, 0.8, {y: 100, opacity: 0})
+.staggerFrom(navItems, 0.5, {y: -50, opacity: 0}, 0.2, '-=1')
 
 transition.play() ;
---
+++ assets/js/usersInterface.js
@@ -0,0 +1,9 @@
+/* Set the width of the dah-nav to 250px (show it) */
+function openNav() {
+    document.getElementById("dash").style.width = "250px";
+  }
+  
+  /* Set the width of the dah-nav to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("dash").style.width = "0px";
+  }
